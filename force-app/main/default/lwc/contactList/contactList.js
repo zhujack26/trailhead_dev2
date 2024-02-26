ce53cf7414f26a04de7f5ed9966fe3e95b8fbc4d
JuseungPark <zhujack26@gmail.com>
2024-02-26 11:21:08 +0900

    Handle Server Errors

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -3,6 +3,8 @@ import FirstName_FIELD from '@salesforce/schema/Contact.FirstName';
 import LastName_FIELD from '@salesforce/schema/Contact.LastName';
 import Email_FIELD from '@salesforce/schema/Contact.Email';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
+import { reduceErrors } from 'c/ldsUtils';
+
 const COLUMNS = [
     { label: 'FirstName', fieldName: FirstName_FIELD.fieldApiName, type: 'text' },
     { label: 'LastName', fieldName: LastName_FIELD.fieldApiName, type: 'text' },
@@ -12,4 +14,9 @@ export default class ContactList extends LightningElement {
     columns = COLUMNS;
     @wire(getContacts)
     contacts;
+
+    get errors() {
+        return (this.contacts.error) ?
+            reduceErrors(this.contacts.error) : [];
+    }
 }
